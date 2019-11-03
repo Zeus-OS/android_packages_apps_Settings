afdb84cf07f2af907721e86b7cdceeafdbd8ede1
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-11-03 09:12:03 +0100

    Revert "Remove Permissions Hub."
    This reverts commit 1e7c172ad5704754266bee50a9bc59b3205e8b70.
    
    Change-Id: Iee02884f139405ae499a6e23de91d3c9c168a255
    
    # Conflicts:
    #	src/com/android/settings/location/RecentLocationAccessPreferenceController.java

--- src/com/android/settings/location/RecentLocationAccessPreferenceController.java
+++ src/com/android/settings/location/RecentLocationAccessPreferenceController.java
@@ -64,8 +64,9 @@ public class RecentLocationAccessPreferenceController extends AbstractPreference
 
     @Override
     public boolean isAvailable() {
-        return DeviceConfig.getBoolean(DeviceConfig.NAMESPACE_PRIVACY,
-                Utils.PROPERTY_PERMISSIONS_HUB_ENABLED, true);
+        return Boolean.parseBoolean(
+                DeviceConfig.getProperty(DeviceConfig.NAMESPACE_PRIVACY,
+                        Utils.PROPERTY_PERMISSIONS_HUB_ENABLED));
     }
 
     @Override
