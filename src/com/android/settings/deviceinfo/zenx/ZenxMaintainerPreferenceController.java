d0820a717936864723d8ef5fc539cc63a95f50ec
Jackeagle <jackeagle102@gmail.com>
2020-06-25 10:52:51 +0200

    Settings: Switch to string overlay for maintainer string
    - Define following overlay in your device tree
        <string name="zenx_maintainer"></string>

--- src/com/android/settings/deviceinfo/zenx/ZenxMaintainerPreferenceController.java
+++ src/com/android/settings/deviceinfo/zenx/ZenxMaintainerPreferenceController.java
@@ -17,7 +17,7 @@
 package com.android.settings.deviceinfo.zenx;
 
 import android.content.Context;
-import android.os.SystemProperties;
+import android.content.res.Resources;
 import android.text.TextUtils;
 
 import androidx.preference.Preference;
@@ -28,7 +28,6 @@ import com.android.settings.core.BasePreferenceController;
 public class ZenxMaintainerPreferenceController extends BasePreferenceController {
 
     private static final String TAG = "ZenxMaintainerPreferenceController";
-    private static final String ROM_PROPERTY = "ro.zenx.maintainer";
 
     public ZenxMaintainerPreferenceController(Context context, String key) {
         super(context, key);
@@ -39,8 +38,7 @@ public class ZenxMaintainerPreferenceController extends BasePreferenceController
     }
 
     public CharSequence getSummary() {
-        String rom = SystemProperties.get(ROM_PROPERTY,
-                this.mContext.getString(R.string.device_info_default));
-        return rom;
+        String maintainer = mContext.getResources().getString(R.string.zenx_maintainer);
+        return maintainer;
     }
 }
