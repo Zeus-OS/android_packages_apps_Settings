9624061bb691b68e5c3644b29bddda4cd68f6dd9
ZeNiXxX <zenixxx.havoc@gmail.com>
2020-12-12 11:34:08 +0100

    Settings: Move out and use better place for some Developer options[1/2]
    also cleanup some preferences

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -32,6 +32,8 @@ import com.android.settings.display.TapToWakePreferenceController;
 import com.android.settings.display.ThemePreferenceController;
 import com.android.settings.display.TimeoutPreferenceController;
 import com.android.settings.display.VrDisplayPreferenceController;
+import com.android.settings.development.DefaultLaunchPreferenceController;
+import com.android.settings.development.EmulateDisplayCutoutPreferenceController;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.core.AbstractPreferenceController;
 import com.android.settingslib.core.lifecycle.Lifecycle;
@@ -95,6 +97,9 @@ public class DisplaySettings extends DashboardFragment {
         controllers.add(new ShowOperatorNamePreferenceController(context));
         controllers.add(new ThemePreferenceController(context));
         controllers.add(new BrightnessLevelPreferenceController(context, lifecycle));
+        controllers.add(new DefaultLaunchPreferenceController(context, "density"));
+        controllers.add(new EmulateDisplayCutoutPreferenceController(context));
+
         return controllers;
     }
 
