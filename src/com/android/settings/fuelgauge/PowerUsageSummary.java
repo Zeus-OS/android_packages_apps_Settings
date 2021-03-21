69c84dbca18039776f8fc822727c6b98b3465915
ZeNiXxX <zenixxx.havoc@gmail.com>
2021-03-21 12:34:23 +0100

    Settings: Revert Battery redesign

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -206,10 +206,12 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
     protected void updateViews(List<BatteryInfo> batteryInfos) {
         final BatteryMeterView batteryView = mBatteryLayoutPref
                 .findViewById(R.id.battery_header_icon);
+        final TextView percentRemaining =
+                mBatteryLayoutPref.findViewById(R.id.battery_percent);
         final TextView summary1 = mBatteryLayoutPref.findViewById(R.id.summary1);
         BatteryInfo oldInfo = batteryInfos.get(0);
         BatteryInfo newInfo = batteryInfos.get(1);
-        batteryView.setText(Utils.formatPercentage(oldInfo.batteryLevel));
+        percentRemaining.setText(Utils.formatPercentage(oldInfo.batteryLevel));
 
         // set the text to the old estimate (copied from battery info). Note that this
         // can sometimes say 0 time remaining because battery stats requires the phone
@@ -511,15 +513,13 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
 
     @VisibleForTesting
     void initHeaderPreference() {
-        if (getContext() != null) {
-            final BatteryMeterView batteryView = (BatteryMeterView) mBatteryLayoutPref
-                  .findViewById(R.id.battery_header_icon);
-            final TextView timeText = (TextView) mBatteryLayoutPref.findViewById(R.id.battery_percent);
+        final BatteryMeterView batteryView = (BatteryMeterView) mBatteryLayoutPref
+                .findViewById(R.id.battery_header_icon);
+        final TextView timeText = (TextView) mBatteryLayoutPref.findViewById(R.id.battery_percent);
 
-            batteryView.setBatteryLevel(mBatteryLevel);
-            batteryView.setPowerSave(mPowerManager.isPowerSaveMode());
-            timeText.setText(formatBatteryPercentageText(mBatteryLevel));
-        }
+        batteryView.setBatteryLevel(mBatteryLevel);
+        batteryView.setPowerSave(mPowerManager.isPowerSaveMode());
+        timeText.setText(formatBatteryPercentageText(mBatteryLevel));
     }
 
     @VisibleForTesting
@@ -539,7 +539,7 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
                     final Integer level = (Integer) animation.getAnimatedValue();
                     batteryView.setBatteryLevel(level);
                     batteryView.setPowerSave(mPowerManager.isPowerSaveMode());
-                    batteryView.setText(formatBatteryPercentageText(level));
+                    timeTextView.setText(formatBatteryPercentageText(level));
                 }
             });
             animator.start();
